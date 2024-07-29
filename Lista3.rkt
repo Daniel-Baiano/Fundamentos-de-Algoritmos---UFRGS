e6b789892ba6098bcac36ea31db8e7d289d8940f
yasbiotec <167362343+yasbiotec@users.noreply.github.com>
2024-07-29 19:34:36 -0300

    Add files via upload

---
+++ Lista3.rkt
@@ -0,0 +1,180 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname Lista3) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+;#####################################################################################################
+;;
+;; LISTA AVALIADA 3
+;;
+;#####################################################################################################
+;; IDENTIFICAÇÃO DO GRUPO
+;; Escreva abaixo, em ordem alfabética, o nome e número de matrícula de todos os membros do grupo:
+;;    Nome Sobrenome Matrícula
+;; 1.
+;; 2.
+;; 3.
+;; 4.
+;#####################################################################################################
+
+;; ============================================
+;; DEFINIÇÕES DE TIPOS DE DADOS (não modificar)
+;; ============================================
+
+;; ------------------
+;; TIPO GENERO:
+;; ------------------
+;; Um Genero é
+;; 1. a string "feminino", ou
+;; 2. a string "masculino", ou
+;; 3. a srting "outros"
+
+;; ------------------
+;; TIPO FUNCIONARIO:
+;; ------------------
+(define-struct funcionario (nome genero anos-empresa destaque?))
+;; Um elemento do conjunto Funcionario tem o formato
+;;     (make-funcionario n g a d) onde
+;;      n: String, é o nome do funcionário,
+;;      g: Genero, é o genero do funcionário,
+;;      a: Número, representa os anos de serviço na empresa do funcionário,
+;;      d: Boolean, representa se funcionário foi o destaque o mês.
+
+;; ------------------
+;; TIPO SETOR:
+;; ------------------
+(define-struct setor (nome membros))
+;; Um elemento do conjunto Setor tem o formato
+;;     (make-setor nome lmemb) onde
+;;     nome: String, é o nome do setor e
+;;     lmemb: Lista-membros, é uma lista de membros deste setor.
+
+;; ------------------
+;; TIPO LISTA-MEMBROS:
+;; ------------------
+;; Uma Lista-membros é
+;; 1. empty (vazia), ou
+;; 2. (cons f lm), onde f : Funcionario, e lm : Lista-membros, ou
+;; 3. (cons s lm), onde s : Setor, e  lm : Lista-membros.
+
+;; =========================================================================
+;;                                 QUESTÃO 1
+;; =========================================================================
+
+;;Funcionarios:
+(define PEDRO (make-funcionario "Pedro"
+                                "Masculino"
+                                20
+                               #true))
+(define ANTONI (make-funcionario "Antoni"
+                                "Outros"
+                                10
+                               #true))
+(define MATEO (make-funcionario "Mateo"
+                                "Masculino"
+                                5
+                               #false))
+(define MARIA (make-funcionario "Maria"
+                                "Feminino"
+                                15
+                               #true))
+(define JULIA (make-funcionario "Julia"
+                                "Feminino"
+                                30
+                               #false))
+(define MARCOS (make-funcionario "Marcos"
+                                "Masculino"
+                                4
+                               #false))
+(define PAMELA (make-funcionario "Pamela"
+                                "Outros"
+                                1
+                               #false))
+(define HARLEY (make-funcionario "Harley"
+                                "feminino"
+                                5
+                               #false))
+(define BRUCE (make-funcionario "Bruce"
+                                "Masculino"
+                                1
+                               #true))
+(define MARK (make-funcionario "Mark"
+                                "Masculino"
+                                1
+                               #false))
+(define LUARA (make-funcionario "Luara"
+                                "Outros"
+                                9
+                               #true))
+(define JAQUI (make-funcionario "Jaqui"
+                                "Masculino"
+                                3
+                               #false))
+(define YUNY (make-funcionario "Yuny"
+                                "Outros"
+                                5
+                               #false))
+(define MIGUEL (make-funcionario "Miguel"
+                                "Outros"
+                                1
+                               #true))
+(define MURI (make-funcionario "Muri"
+                                "Masculino"
+                                3
+                               #false))
+(define RENATA (make-funcionario "Renata"
+                                "Feminino"
+                                3
+                               #false))
+;;sub-Setores:
+(define DEV (make-setor "Desenvolvimento"(list PEDRO ANTONI)))
+(define QA(make-setor "Game tester" (list HARLEY MARCOS)))
+(define ARTE_CONCEITUAL(make-setor "Artista Conceitual" (list LUARA JAQUI)))
+(define ANIMACAO(make-setor "Animação 2D" (list MARK BRUCE)))
+(define MIDIA(make-setor "Midia social" (list PAMELA MATEO)))
+(define CONTEUDO(make-setor "Marketing de Conteúdo"(list MARIA JULIA)))
+(define TRILHA_SONORA(make-setor "Trilha Sonora do jogo"(list YUNY MURI)))
+(define DUB(make-setor "Dubladores"(list MIGUEL RENATA)))
+
+;;SETORES:
+(define GRAPH (make-setor "Graficos do game"(list ARTE_CONCEITUAL ANIMACAO)))
+(define PROG (make-setor "Programação do game"(list DEV QA)))
+(define MARKETING (make-setor "Marketing do game"(list MIDIA CONTEUDO))) 
+(define SONS(make-setor "Trilha sonora do game"(list TRILHA_SONORA DUB)))
+;; =========================================================================
+;;                                 QUESTÃO 2
+;; =========================================================================
+;;Para fazer uma análise de seus funcionários, a empresa quer saber quantos
+;;funcionários há do gênero feminino, gênero masculino ou outro. Construa a função
+;;quantidade-por-genero que, dados um setor e um gênero (use as definições de dados do
+;;template para esses tipos), devolve o número de funcionários deste gênero neste setor
+;;da empresa.
+
+
+;; quantidade-por-genero: @Setor genero -> Numero
+;;Dado um Setor e um genero, devolve o numero de pessoas com aquele genero no setor
+(define (quantidade-por-genero @s genero)
+  (cond
+    [(empty? @s)+0]
+    [(string=?(funcionario-genero funcionario) genero)+1]))
+    
+
+;; =========================================================================
+;;                                 QUESTÃO 3
+;; =========================================================================
+;; maximo: ..... -> .....
+
+;; =========================================================================
+;;                                 QUESTÃO 4
+;; =========================================================================
+;; grau: ..... -> ......
+
+;; altura: ..... -> ......
+
+;; =========================================================================
+;;                                 QUESTÃO 5
+;; =========================================================================
+;; gera-imagem-setor: ..... -> ......
+
+;; =========================================================================
+;;                                 QUESTÃO 6
+;; =========================================================================
+;; insere-funcionario: ..... -> ......
